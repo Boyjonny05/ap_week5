4eaac25e99de999fbfcc1fb2af0ad154ec3a6bb7
Boyjonny05 <Void59315@gmail.com>
2025-11-07 14:49:44 +0000

    Clean Code

---
+++ problem_set_2.py
@@ -0,0 +1,6 @@
+def problem2():
+    # Problem Set 2: Extracting Information
+    # From Descriptions:
+    # Extract the name of the famous personality from the quote "Ask not what your country can do for you — ask what you can do for your country. - John F. Kennedy"
+    quote = "Ask not what your country can do for you — ask what you can do for your country. - John F. Kennedy"
+    print(quote.split(' - ')[1])
